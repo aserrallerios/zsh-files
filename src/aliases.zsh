8e2f0f4a077cb45c5855233a6340ae929cf57c40
Albert Serrallé Ríos <aserrallerios@gmail.com>
2018-06-18 14:37:21 +0200

    Add print colors alias

--- src/aliases.zsh
+++ src/aliases.zsh
@@ -12,6 +12,7 @@ alias netstatl="netstat -lnptu"
 alias lsofil="lsof -i -n | egrep 'COMMAND|LISTEN'"
 alias nmap-discover='sudo nmap -PR -sP 192.168.1.0/24'
 alias myip="curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'"
+alias colorprint='for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"'
 
 alias docker-rmi-none="sudo docker rmi \`sudo docker images | grep none | cut -f1 -d$'\t'\`"
 
