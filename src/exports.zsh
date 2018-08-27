638970b6c419960a93f877a6fc887dc3105e7fc7
Albert Serrallé Ríos <aserrallerios@gmail.com>
2018-08-27 10:29:18 +0200

    Remove app specific stuff

--- src/exports.zsh
+++ src/exports.zsh
@@ -1,10 +1,7 @@
 # Yes, really
 export PATH=$PATH:~/.bin
+export PATH=$PATH:~/.local/bin
 export PATH=$PATH:/sbin:/usr/sbin
-
-# Currently this path is appendend to dynamically when picking a ruby version
-export PATH=node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:~/.cabal/bin:~/.local/bin:$PATH
-export PATH=$PATH:/opt/boxen/homebrew/opt/go/libexec/bin
 
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
@@ -26,22 +23,3 @@ export LC_COLLATE=C
 
 export DRIP_SHUTDOWN=30
 export EDITOR=vim
-
-# GO lang
-export PATH=$PATH:/usr/local/go/bin:~/bin
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-
-# Ruby rbenv
-export PATH=$PATH:$HOME/.rbenv/bin:$PATH
-
-# npm
-export NPM_PACKAGES=$HOME/.npm-packages
-export NODE_PATH=$NODE_PATH:$NPM_PACKAGES/lib/node_modules
-export PATH=$PATH:$NPM_PACKAGES/bin
-# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-# unset MANPATH # delete if you already modified MANPATH elsewhere in your config
-export MANPATH=$(manpath):$NPM_PACKAGES/share/man
-
-# sdkman
-export SDKMAN_DIR=$HOME/.sdkman
