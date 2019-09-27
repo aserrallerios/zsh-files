6aa3be3699e49d2c51931b082082658d58c51429
Albert Serrallé Ríos <aserrallerios@gmail.com>
2019-09-27 18:56:39 +0200

    Fix slow paste

--- src/completion.zsh
+++ src/completion.zsh
@@ -63,3 +63,6 @@ zstyle ':filter-select' max-lines -10 # use $LINES - 10 for filter-select
 zstyle ':filter-select' rotate-list yes # enable rotation for filter-select
 zstyle ':filter-select' case-insensitive yes # enable case-insensitive search
 zstyle ':filter-select' extended-search no # see below
+
+# Fix slow paste
+zstyle ':bracketed-paste-magic' active-widgets '.self-*'
