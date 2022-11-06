b67ace0e5799c4b553c8100c03622673733207c0
Albert Serrallé Ríos <aserrallerios@gmail.com>
2022-11-06 10:23:50 +0100

    Remove chruby stuff

--- src/ruby/init.zsh
+++ src/ruby/init.zsh
@@ -2,10 +2,3 @@
 if [ -x "$(command -v gem)" ]; then
   PATH="${$(gem environment gempath)/://bin:}/bin":$PATH
 fi
-
-if [ -e "/usr/share/chruby/chruby.sh" ]; then
-    source /usr/share/chruby/chruby.sh
-fi
-
-# Default to ruby other than the system ruby
-chruby `chruby | tr -d '* ' | sort -r`
