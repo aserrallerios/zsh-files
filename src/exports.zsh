2ffe8e67eccb10c2f395a96457ce4c1cc8aa9259
Albert Serrallé Ríos <aserrallerios@gmail.com>
2023-08-01 22:20:33 +0200

    Update exports.zsh
    Remove arch variable, that depends on the system.
    
    It'll be moved to `zshrc.`

--- src/exports.zsh
+++ src/exports.zsh
@@ -12,9 +12,6 @@ export TERM=xterm-256color
 # Enable color in grep
 export GREP_COLOR='1;33'
 
-# This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
-export ARCHFLAGS='-arch x86_64'
-
 export LESS='-rFnXM --ignore-case --raw-control-chars'
 # export PAGER='most'
 export PAGER='less'
